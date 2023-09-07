a1d08fb00eb8c6df83c38751f46add3eda10d583
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-09-07 21:51:01 +0200

    Facade - home theatre

---
+++ src/main/java/head_first_desing_patterns/section_7_adapter_facade/facade/HomeTheaterFacade.java
@@ -0,0 +1,66 @@
+package head_first_desing_patterns.section_7_adapter_facade.facade;
+
+public class HomeTheaterFacade {
+    private Amplifier amp;
+    private Tuner tuner;
+    private DvdPlayer dvd;
+    private CdPlayer cd;
+    private Projector projector;
+    private TheaterLights lights;
+    private Screen screen;
+    private PopcornPopper popper;
+
+    public HomeTheaterFacade(
+            Amplifier amp,
+            Tuner tuner,
+            DvdPlayer dvd,
+            CdPlayer cd,
+            Projector projector,
+            TheaterLights lights,
+            Screen screen,
+            PopcornPopper popper) {
+        this.amp = amp;
+        this.tuner = tuner;
+        this.dvd = dvd;
+        this.cd = cd;
+        this.projector = projector;
+        this.lights = lights;
+        this.screen = screen;
+        this.popper = popper;
+    }
+
+    public void watchMovie(String movie){
+        System.out.println("Get ready to watch a movie... ");
+
+        popper.on();
+        popper.pop();
+
+        lights.dim(10);
+        screen.down();
+
+        projector.on();
+        projector.wideScreenMode();
+
+        amp.on();
+        amp.setDvd(dvd);
+        amp.setSurroundSound();
+        amp.setVolume(5);
+
+        dvd.on();
+        dvd.play(movie);
+
+    }
+
+    public void endMovie(){
+        System.out.println("Shutting movie theatre down...");
+
+        popper.off();
+        lights.on();
+        screen.up();
+        projector.off();
+        amp.off();
+        dvd.eject();
+        dvd.off();
+
+    }
+}
